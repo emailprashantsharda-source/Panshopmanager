0c6d9b870406cc5a067ad811eec5af77f40014b8
emailprashantsharda-source <emailprashantsharda@gmail.com>
2026-06-28 13:09:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * DEPLOY RULE: bump SW_VERSION every deploy (set it to window.HISAABNOW_BUILD).
  */
 
-const SW_VERSION  = 'v33_166_menu_route_fix';
+const SW_VERSION  = 'v33_167_checklist_persist';
 const CACHE       = 'hisaabnow-' + SW_VERSION;
 const NAV_TIMEOUT = 4000; /* ms before falling back to cached HTML */
 
