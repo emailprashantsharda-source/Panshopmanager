70b431a7ba0044da1197d849ba47cac5cc5ebc6f
emailprashantsharda-source <emailprashantsharda@gmail.com>
2026-07-06 18:23:08 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * DEPLOY RULE: bump SW_VERSION every deploy (set it to window.HISAABNOW_BUILD).
  */
 
-const SW_VERSION  = 'v33_339_desktop_channels_tables';
+const SW_VERSION  = 'v33_341_cust_phone_autodetect';
 const CACHE       = 'hisaabnow-' + SW_VERSION;
 const NAV_TIMEOUT = 4000; /* ms before falling back to cached HTML */
 
