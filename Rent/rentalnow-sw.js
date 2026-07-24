b6b41592eb0425faa9edf8f1090b4f624ab59626
emailprashantsharda-source <emailprashantsharda@gmail.com>
2026-07-24 16:41:47 +0530

    Add files via upload

--- Rent/rentalnow-sw.js
+++ Rent/rentalnow-sw.js
@@ -3,7 +3,7 @@
  * shell must never outlive a new build. Cache is the offline fallback
  * only, after NAV_TIMEOUT.
  */
-const SW_VERSION = 'rn_v3_1_0_paymode';
+const SW_VERSION = 'rn_v3_2_0_brand';
 const CACHE = 'rentalnow-' + SW_VERSION;
 const NAV_TIMEOUT = 4000;
 const SHELL = ['./rentalnow.html'];
