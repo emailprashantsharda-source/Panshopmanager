17c055c3e395c229e14d07a5be8233440bc063e7
emailprashantsharda-source <emailprashantsharda@gmail.com>
2026-09-19 20:20:45 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@
  * never installs anything new, no matter how often it's polled).
  */
 
-const SW_VERSION  = 'v34_auto_20260918_1406';
+const SW_VERSION  = 'v34_auto_20260919_2017';
 const CACHE       = 'hisaabnow-' + SW_VERSION;
 const NAV_TIMEOUT = 4000; /* ms before falling back to cached HTML */
 
